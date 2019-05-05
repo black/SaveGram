7b530eea149b8064fbe7cc6cd03574d6c358768e
Λ B H I N Λ V <ixd.abhi@gmail.com>
2019-05-06 00:00:01 +0530

    Getting data from Clipboard correctly

--- app/src/main/java/com/example/savegram/MainActivity.java
+++ app/src/main/java/com/example/savegram/MainActivity.java
@@ -1,10 +1,19 @@
 package com.example.savegram;
 
+import android.Manifest;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.MenuItem;
+import android.webkit.URLUtil;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -37,6 +46,44 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        //Permission Check
+        int PERMISSIONS_ALL = 1;
+        String[] permissions = {
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA,Manifest.permission.RECORD_AUDIO
+        };
+        if (!hasPermissions(this, permissions)) {
+            ActivityCompat.requestPermissions(this,permissions, PERMISSIONS_ALL);
+        }
+
+        String clip = getClipboardURL();
+        Log.d("URL",clip);
+        if(URLUtil.isValidUrl(clip)){
+            Log.d("URL","True: " + clip);
+        }else{
+            Log.d("URL","False: No url found");
+        }
+    }
+
+    public static boolean hasPermissions(Context context, String... permissions){
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M && context!=null && permissions!=null){
+            for (String permission:permissions){
+                if (ActivityCompat.checkSelfPermission(context,permission)!= PackageManager.PERMISSION_GRANTED){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private String getClipboardURL(){
+        ClipboardManager clipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+        ClipData clip =  clipboardManager.getPrimaryClip();
+        if (clip != null && clip.getItemCount() > 0) {
+            return clip.getItemAt(0).coerceToText(this).toString();
+        }
+        return null;
     }
 
 }
